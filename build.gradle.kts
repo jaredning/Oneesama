eeddfd9b905475705cb49b291e880cd5f8572432
Jared Ning <jared.ning@yahoo.com>
2026-04-04 04:34:08 -0400

    Fix build issues after merge

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,10 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("io.realm:realm-gradle-plugin:10.19.0")
-    }
-}
-
 plugins {
     alias(libs.plugins.android.application) apply false
+}
+
+buildscript {
+    dependencies {
+        classpath(libs.realm.gradle.plugin)
+    }
 }
