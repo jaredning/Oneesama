1f03bfa79ab638eb6043020597fc9246725e11f3
Jared Ning <jared.ning@yahoo.com>
2026-04-05 02:19:53 -0400

    Fix broken test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,15 +11,16 @@ android {
         applicationId = "catgirl.oneesama.v2"
         minSdk = 23
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
